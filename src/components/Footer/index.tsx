4215097ffb7b3352b6689879938defd410f0a419
shuai <lishuailing@sifou.com>
2022-10-13 18:19:28 +0800

    fix: footer add icons link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,20 +9,24 @@ const Footer:FC = () => {
     <footer className="footer">
       <div className='container padding-vert--md'>
         <div className='row text--secondary justify-center'>
-          <div className='col col--6'>
+          <div className='col col--7'>
             <div style={{  height: '28px', marginBottom: '1rem' }}>
               <DarkSvg role="img" />
             </div>
             {/* <img src={require('@site/static/img/logo-dark.svg').default} alt="" height="28" /> */}
             <div className='fs-14'>
-              <span>
+              <div>
+                <span>
                 Designed and built with all the love in the world by the
-              </span>
-              {' '}
-              <a href="https://github.com/orgs/answerdev/people" target="_blank">Answer team</a>
-              <span> with the help of </span>
-              <a href="https://github.com/answerdev/answer/graphs/contributors" target="_blank">our contributors</a>
-              <span> .Code licensed under the </span>
+                </span>
+                {' '}
+                <a href="https://github.com/orgs/answerdev/people" target="_blank">Answer team</a>
+                <span> with the help of </span>
+                <a href="https://github.com/answerdev/answer/graphs/contributors" target="_blank">our contributors</a>
+                .
+              </div>
+
+              <span>Code licensed under the </span>
               <a
                 href="https://www.apache.org/licenses/LICENSE-2.0"
                 target="_blank"
@@ -30,9 +34,10 @@ const Footer:FC = () => {
                 Apache-2.0 License
               </a>
               .
+              <span> Icons by <a href="https://www.flaticon.com/free-icons/sports-and-competition" target="_blank">Freepik</a>.</span>
             </div>
           </div>
-          <div className={clsx('col col--4', styles.right)}>
+          <div className={clsx('col col--3', styles.right)}>
             <a href="/" className='text--secondary margin-right--md'>Twitter</a>
             <a href="https://github.com/answerdev/answer" target="_blank" className='text--secondary'>GitHub</a>
           </div>
