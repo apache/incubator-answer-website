579982297dd39d03ccce31a9b8dceb1b801b5bba
shuai <lishuailing@sifou.com>
2023-04-11 17:54:38 +0800

    fix: change index page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,16 @@ import Layout from '@theme/Layout';
 import HomePageHeader from '@site/src/components/HomePageHeader';
 import HomepageMain from '@site/src/components/HomepageMain';
 import Footer from '@site/src/components/Footer';
+import Head from '@docusaurus/Head';
 
 
 export default function Home(): JSX.Element {
   return (
     <Layout
       description="An open-source knowledge-based community software. You can use it quickly to build Q&A community for your products, customers, teams, and more.">
-
+      <Head>
+        <title>Answer | Q&A Platforms</title>
+      </Head>
       <main className='headerBg'>
         <HomePageHeader />
         <HomepageMain />
