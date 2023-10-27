4c760d508d07abd5040890381e75f77198fbb160
sy-records <52o@qq52o.cn>
2023-10-27 09:10:43 +0800

    update: complete resource localization

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -147,14 +147,8 @@ const config = {
       },
     }),
   stylesheets: [
-    'https://fonts.googleapis.com/css2?family=Lexend:wght@400..700&display=swap',
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
+    '/fonts/fonts.css',
+    '/katex/katex.min.css',
   ],
   plugins: [
     // Use custom blog plugin
