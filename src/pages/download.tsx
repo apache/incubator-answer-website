2c2817091e95793e54e73443083fedc6b9a00abc
Fen <f3nb0x@gmail.com>
2024-05-31 12:04:36 +0800

    Update: Download page (#178)

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -7,104 +7,104 @@ import HeaderSlogan from '../components/HeaderSlogan';
 
 
 export default function Plugins(): JSX.Element {
-    const [] = React.useState([]);
-    const list:string[] = ["1.3.1","1.3.0", "1.2.5", "1.2.1", "1.2.0"]
-    let table:JSX.Element = list.map((item: string) => {
-        return (
+  let item = "1.3.1";
+  let table:JSX.Element = () => {
+  };
+  return (
+    <Layout
+      title={translate({message: 'Download'})}
+      description="This page provides download links for the latest release of Apache Answer.">
+      <HeaderSlogan type="download"/>
+      <Container className='py-5'>
+        <h3>The Latest Release</h3>
+        <br />
+        <table>
+          <thead>
             <tr>
-                <td>{item}</td>
-                <td>[<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-src.tar.gz`}
-                    target="_blank" rel="noopener noreferrer">Code</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-src.tar.gz.asc`}
-                    target="_blank" rel="noopener noreferrer">Sign</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-src.tar.gz.sha512`}
-                    target="_blank" rel="noopener noreferrer">SHA512</a>]
-                </td>
-                <td>[<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-darwin-amd64.tar.gz`}
-                    target="_blank" rel="noopener noreferrer">Binary</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-darwin-amd64.tar.gz.asc`}
-                    target="_blank" rel="noopener noreferrer">Sign</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-darwin-amd64.tar.gz.sha512`}
-                    target="_blank" rel="noopener noreferrer">SHA512</a>]
-                </td>
-                <td>[<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-darwin-arm64.tar.gz`}
-                    target="_blank" rel="noopener noreferrer">Binary</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-darwin-arm64.tar.gz.asc`}
-                    target="_blank" rel="noopener noreferrer">Sign</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-darwin-arm64.tar.gz.sha512`}
-                    target="_blank" rel="noopener noreferrer">SHA512</a>]
-                </td>
-                <td>[<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-amd64.tar.gz`}
-                    target="_blank" rel="noopener noreferrer">Binary</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-amd64.tar.gz.asc`}
-                    target="_blank" rel="noopener noreferrer">Sign</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-amd64.tar.gz.sha512`}
-                    target="_blank" rel="noopener noreferrer">SHA512</a>]
-                </td>
-                <td>[<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-arm64.tar.gz`}
-                    target="_blank" rel="noopener noreferrer">Binary</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-arm64.tar.gz.asc`}
-                    target="_blank" rel="noopener noreferrer">Sign</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-arm64.tar.gz.sha512`}
-                    target="_blank" rel="noopener noreferrer">SHA512</a>]
-                </td>
-                <td>[<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-windows-amd64.tar.gz`}
-                    target="_blank" rel="noopener noreferrer">Binary</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-windows-amd64.tar.gz.asc`}
-                    target="_blank" rel="noopener noreferrer">Sign</a>] [<a
-                    href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-windows-amd64.tar.gz.sha512`}
-                    target="_blank" rel="noopener noreferrer">SHA512</a>]
-                </td>
+              <th>Version</th>
+              <th>Date</th>
+              <th>Download</th>
+              <th>Release Notes</th>
             </tr>
-        )
-    })
-    return (
-        <Layout
-            title={translate({message: 'Download'})}
-            description="This page provides download links for the latest release of Apache Answer.">
-            <HeaderSlogan type="download"/>
-            <Container className='py-5'>
-                <h3>Release Source And Binary</h3>
-                <br/>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>version</th>
-                        <th>source</th>
-                        <th>darwin-amd64</th>
-                        <th>darwin-arm64</th>
-                        <th>linux-amd64</th>
-                        <th>linux-arm64</th>
-                        <th>windows-amd64</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {table}
-                    </tbody>
-                </table>
-                <div className='mt-5'>
-                    <h3>Release Integrity</h3>
-                    <p>You can verify the integrity of the downloaded files. </p>
-                    <ul>
-                        <li>
-                            <a href="https://answer.apache.org/community/how-to-release/#how-to-verify-the-signatures">How to verify the signatures</a>
-                        </li>
-                        <li>
-                            <a href="https://answer.apache.org/community/how-to-release#how-to-verify-the-checksums">How to verify the checksums</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className='mt-5'>
-                    <h3>Release Docker Image</h3>
-                    <p>Apache Answer provides a docker image for each release. You can pull the image from the <a href="https://hub.docker.com/r/apache/answer/tags" target="_blank" rel="noopener noreferrer">Docker Hub</a>.</p>
-                </div>
-            </Container>
-        </Layout>
-    )
+          </thead>
+          <tbody>
+            <tr>
+              <td>{item}</td>
+              <td>2024-05-27</td>
+              <td>
+                <ul className='list-unstyled'>
+                  <li>
+                    <a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-src.tar.gz`}
+            target="_blank" rel="noopener noreferrer">apache-answer-{item}-incubating-src.tar.gz</a> (<a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-src.tar.gz.asc`}
+            target="_blank" rel="noopener noreferrer">SIGN</a>, <a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-src.tar.gz.sha512`}
+            target="_blank" rel="noopener noreferrer">SHA512</a>)
+                  </li>
+                  <li>
+                    <a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-darwin-arm64.tar.gz`}
+            target="_blank" rel="noopener noreferrer">apache-answer-{item}-incubating-bin-darwin-arm64.tar.gz</a> (<a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-darwin-arm64.tar.gz.asc`}
+            target="_blank" rel="noopener noreferrer">SIGN</a>, <a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-darwin-arm64.tar.gz.sha512`}
+            target="_blank" rel="noopener noreferrer">SHA512</a>)
+                  </li>
+                  <li>
+                    <a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-amd64.tar.gz`}
+            target="_blank" rel="noopener noreferrer">apache-answer-{item}-incubating-bin-linux-amd64.tar.gz</a> (<a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-amd64.tar.gz.asc`}
+            target="_blank" rel="noopener noreferrer">SIGN</a>, <a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-amd64.tar.gz.sha512`}
+            target="_blank" rel="noopener noreferrer">SHA512</a>)
+                  </li>
+                  <li>
+                    <a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-arm64.tar.gz`}
+            target="_blank" rel="noopener noreferrer">apache-answer-{item}-incubating-bin-linux-arm64.tar.gz</a> (<a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-arm64.tar.gz.asc`}
+            target="_blank" rel="noopener noreferrer">SIGN</a>, <a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-linux-arm64.tar.gz.sha512`}
+            target="_blank" rel="noopener noreferrer">SHA512</a>)
+                  </li>
+                  <li>
+                    <a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-windows-amd64.tar.gz`}
+            target="_blank" rel="noopener noreferrer">apache-answer-{item}-incubating-bin-windows-amd64.tar.gz</a> (<a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-windows-amd64.tar.gz.asc`}
+            target="_blank" rel="noopener noreferrer">SIGN</a>, <a
+            href={`https://downloads.apache.org/incubator/answer/${item}-incubating/apache-answer-${item}-incubating-bin-windows-amd64.tar.gz.sha512`}
+            target="_blank" rel="noopener noreferrer">SHA512</a>)
+                  </li>
+                </ul>
+              </td>
+              <td><a href={`https://github.com/apache/incubator-answer/releases/tag/v${item}`}>Release Notes</a></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className='mt-5'>
+          <h3>All Archived Releases</h3>
+          <p>For older releases, please check the <a target="_blank" rel="noopener noreferrer" href="https://archive.apache.org/dist/incubator/answer/">archive</a>.</p>
+        </div>
+        <div className='mt-5'>
+          <h3>Release Integrity</h3>
+          <p>You can verify the integrity of the downloaded files. </p>
+          <ul>
+            <li>
+              <a href="https://answer.apache.org/community/how-to-release/#how-to-verify-the-signatures">How to verify the signatures</a>
+            </li>
+            <li>
+              <a href="https://answer.apache.org/community/how-to-release/#how-to-verify-the-checksums">How to verify the checksums</a>
+            </li>
+          </ul>
+        </div>
+        <div className='mt-5'>
+          <h3>Release Docker Image</h3>
+          <p>Apache Answer provides a docker image for each release. You can pull the image from the <a href="https://hub.docker.com/r/apache/answer/tags" target="_blank" rel="noopener noreferrer">Docker Hub</a>.</p>
+        </div>
+      </Container>
+    </Layout>
+  )
 }
