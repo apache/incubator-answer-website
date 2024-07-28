43dbf5deca6113a534903e21af8d0828fd0971a9
Craig L Russell <apache.clr@gmail.com>
2024-07-28 14:30:41 +0800

    Update index.tsx to fix grammar (#225)
    Change "at scales" to "at scale"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import Head from '@docusaurus/Head';
 export default function Home(): JSX.Element {
   return (
     <Layout
-      description="A Q&A platform software for teams at any scales. Whether it’s a community forum, help center, or knowledge management platform, you can always count on Answer.">
+      description="A Q&A platform software for teams at any scale. Whether it’s a community forum, help center, or knowledge management platform, you can always count on Answer.">
       <Head>
         <title>Apache Answer | Free Open-source Q&A Platform</title>
         <meta data-rh="true" name="twitter:card" content="summary" />
